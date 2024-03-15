6fc0190bcbd11c001451b4ccef8d4cdf60e3f7e5
meriem matnoussi <meriem.matnoussi123@gmail.com>
2024-03-15 07:13:52 -0700

    task 6 and 7

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''to_kv function'''
+from typing import Tuple, Union
+from math import sqrt
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''function to_kv that takes a string k and
+    an int OR float v as arguments and returns a tuple. '''
+    square: float = v ** 2
+    return (k, square)
